eba9e3ece72f100a6e4001f1f9f097460b0eb724
prajnak1 <126357555+prajnak1@users.noreply.github.com>
2025-01-17 23:14:22 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+
+function toggleMenu() {
+    //targeting 2 elements
+    /*
+    const menu = document.querySelector(".menu-links");
+    const icon = document.querySelector(".hamburger-icon");
+    //add & remove open class
+    menu.classList.toggle("open")
+    icon.classList.toggle("open")
+    */
+     
+        const menu = document.querySelector(".menu-links");
+        const icon = document.querySelector(".hamburger-icon");
+        menu.classList.toggle("open");
+        icon.classList.toggle("open");
+      }
+   
+        
+    
+    
+
+    
+        
+    
+
+
